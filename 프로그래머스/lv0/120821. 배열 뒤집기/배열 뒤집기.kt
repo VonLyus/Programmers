5d9992770ddec9f7cef63fad17bfde3be96f4400
AppCode <73988800+VonLyus@users.noreply.github.com>
2023-08-07 13:23:39 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(num_list: IntArray): IntArray {
+        var answer: IntArray = IntArray(num_list.size)
+        for (i in 0 until num_list.size)
+        {
+            answer[i] = num_list[num_list.size - i - 1]
+        }
+        return answer
+    }
+}

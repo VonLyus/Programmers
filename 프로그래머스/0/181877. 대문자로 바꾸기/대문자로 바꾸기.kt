b33e5f8f63de1eafeaa91a60a8f6fccc8b48e54d
LaYuSeong <73988800+VonLyus@users.noreply.github.com>
2024-01-24 10:40:33 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 58.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(myString: String): String {
+        var answer: String = ""
+        
+        answer = myString.toUpperCase()
+        
+        return answer
+    }
+}

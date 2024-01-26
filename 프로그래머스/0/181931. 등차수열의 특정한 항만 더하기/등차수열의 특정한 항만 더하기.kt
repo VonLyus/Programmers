8915c51bc88bce77ab5a773b4790cd0178af3a80
LaYuSeong <73988800+VonLyus@users.noreply.github.com>
2024-01-26 14:00:10 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 61.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(a: Int, d: Int, included: BooleanArray): Int {
+        var answer: Int = 0
+        
+        for(i in included.indices){
+            if(included[i] == true){
+                answer += a + d*i
+            }
+        }
+        return answer
+    }
+}
